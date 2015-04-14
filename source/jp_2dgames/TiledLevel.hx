16ebce4a0dcb5a114865a6a386a4a4e37d4ec156
syun77 <2dgames.jp@gmail.com>
2015-04-15 07:24:00 +0900

    2タブに変更した

--- source/jp_2dgames/TiledLevel.hx
+++ source/jp_2dgames/TiledLevel.hx
@@ -15,115 +15,118 @@ import flixel.group.FlxGroup;
  * TiledMapの読み込みクラス
  **/
 class TiledLevel extends TiledMap {
-    private static inline var C_PATH_LEVEL_TILESHEETS = "assets/levels/";
-    // タイルセットのプロパティ名
-    private static inline var PROPERTY_TILESET = "tileset";
-    // コリジョン無効のプロパティ
-    private static inline var PROPERTY_NOCOLLIDE = "nocollide";
-    // オブジェクトレイヤーかどうか
-    private static inline var PROPERTY_OBJECT = "object";
+	private static inline var C_PATH_LEVEL_TILESHEETS = "assets/levels/";
+	// タイルセットのプロパティ名
+	private static inline var PROPERTY_TILESET = "tileset";
+	// コリジョン無効のプロパティ
+	private static inline var PROPERTY_NOCOLLIDE = "nocollide";
+	// オブジェクトレイヤーかどうか
+	private static inline var PROPERTY_OBJECT = "object";
 
-    public var foregroundTiles:FlxGroup; // 前面レイヤー（描画用）
-    public var backgroundTiles:FlxGroup; // 背面レイヤー（描画用）
+	public var foregroundTiles:FlxGroup; // 前面レイヤー（描画用）
+	public var backgroundTiles:FlxGroup; // 背面レイヤー（描画用）
 
-    private var collidableTileLayers:Array<FlxTilemap>; // コリジョンレイヤー
+	private var collidableTileLayers:Array<FlxTilemap>; // コリジョンレイヤー
 
-    /**
+	/**
      * コンストラクタ
      * @param tileLevel *.tmxファイルパス
      **/
-    public function new(tiledLevel:Dynamic) {
 
-        // *.tmxファイルのロード
-        super(tiledLevel);
+	public function new(tiledLevel:Dynamic) {
 
-        // 前面用グループ作成
-        foregroundTiles = new FlxGroup();
-        // 背景用グループ作成
-        backgroundTiles = new FlxGroup();
+		// *.tmxファイルのロード
+		super(tiledLevel);
 
-        // TMXファイルをレイヤーに展開する
-        // "layers"にレイヤー情報が格納されている
-        for(tileLayer in layers) {
+		// 前面用グループ作成
+		foregroundTiles = new FlxGroup();
+		// 背景用グループ作成
+		backgroundTiles = new FlxGroup();
 
-            if(tileLayer.properties.contains(PROPERTY_OBJECT)) {
-                // オブジェクトレイヤーは何もしない
-                continue;
-            }
+		// TMXファイルをレイヤーに展開する
+		// "layers"にレイヤー情報が格納されている
+		for(tileLayer in layers) {
 
-            // タイルセットとして扱う名前を取得
-            var tileSheetName:String = tileLayer.properties.get(PROPERTY_TILESET);
-            if(tileSheetName == null) {
-                // タイルセットの指定がない
-                throw "Error: 'tileset' property not defined for the '" + tileLayer.name + "' layer. Please add the property to the layer.";
-            }
+			if(tileLayer.properties.contains(PROPERTY_OBJECT)) {
+				// オブジェクトレイヤーは何もしない
+				continue;
+			}
 
-            // タイルセットを探す
-            var tileSet:TiledTileSet = null;
-            for(ts in tilesets) {
-                if(ts.name == tileSheetName) {
-                    // 同名のタイルセットを見つけた
-                    tileSet = ts;
-                    break;
-                }
-            }
+			// タイルセットとして扱う名前を取得
+			var tileSheetName:String = tileLayer.properties.get(PROPERTY_TILESET);
+			if(tileSheetName == null) {
+				// タイルセットの指定がない
+				throw "Error: 'tileset' property not defined for the '" + tileLayer.name + "' layer. Please add the property to the layer.";
+			}
 
-            if(tileSet == null) {
-                // タイルセットが存在しない
-                throw "Error: Tileset '" + tileSheetName + "' not found. Did you mispell the 'tilesheet' property in " + tileLayer.name + "' layer?";
-            }
+			// タイルセットを探す
+			var tileSet:TiledTileSet = null;
+			for(ts in tilesets) {
+				if(ts.name == tileSheetName) {
+					// 同名のタイルセットを見つけた
+					tileSet = ts;
+					break;
+				}
+			}
 
-            // チップ画像のパスを作成
-            var imagePath = new Path(tileSet.imageSource);
-            var processedPath = C_PATH_LEVEL_TILESHEETS + imagePath.file + "." + imagePath.ext;
+			if(tileSet == null) {
+				// タイルセットが存在しない
+				throw "Error: Tileset '" + tileSheetName + "' not found. Did you mispell the 'tilesheet' property in " + tileLayer.name + "' layer?";
+			}
 
-            // FlxTilemapでCSVレイヤーデータを読み込む
-            var tilemap:FlxTilemap = new FlxTilemap();
-            // タイルの幅を設定
-            // CSVでは設定不要
-//            tilemap.widthInTiles = width;
-//            tilemap.heightInTiles = height;
-            // ロード実行
-            tilemap.loadMap(tileLayer.csvData, processedPath, tileSet.tileWidth, tileSet.tileHeight, FlxTilemap.OFF, 1, 1, 1);
+			// チップ画像のパスを作成
+			var imagePath = new Path(tileSet.imageSource);
+			var processedPath = C_PATH_LEVEL_TILESHEETS + imagePath.file + "." + imagePath.ext;
 
-            if(tileLayer.properties.contains(PROPERTY_NOCOLLIDE)) {
+			// FlxTilemapでCSVレイヤーデータを読み込む
+			var tilemap:FlxTilemap = new FlxTilemap();
+			// タイルの幅を設定
+			// CSVでは設定不要
+			//            tilemap.widthInTiles = width;
+			//            tilemap.heightInTiles = height;
+			// ロード実行
+			tilemap.loadMap(tileLayer.csvData, processedPath, tileSet.tileWidth, tileSet.tileHeight, FlxTilemap.OFF, 1, 1, 1);
 
-                // コリジョンなしの場合は背景レイヤー
-                backgroundTiles.add(tilemap);
-            }
-            else {
+			if(tileLayer.properties.contains(PROPERTY_NOCOLLIDE)) {
 
-                // コリジョンありの場合はコリジョンレイヤーに登録
-                if(collidableTileLayers == null) {
-                    collidableTileLayers = new Array<FlxTilemap>();
-                }
+				// コリジョンなしの場合は背景レイヤー
+				backgroundTiles.add(tilemap);
+			}
+			else {
 
-                foregroundTiles.add(tilemap);
-                collidableTileLayers.push(tilemap);
-            }
-        }
-    }
+				// コリジョンありの場合はコリジョンレイヤーに登録
+				if(collidableTileLayers == null) {
+					collidableTileLayers = new Array<FlxTilemap>();
+				}
 
-    /**
+				foregroundTiles.add(tilemap);
+				collidableTileLayers.push(tilemap);
+			}
+		}
+	}
+
+	/**
      * オブジェクトからインスタンスを生成
      **/
-    public function loadObjects(state:PlayState) {
-        for(group in objectGroups) {
-            for(o in group.objects) {
-                _loadObject(o, group, state);
-            }
-        }
-    }
 
-    /**
+	public function loadObjects(state:PlayState) {
+		for(group in objectGroups) {
+			for(o in group.objects) {
+				_loadObject(o, group, state);
+			}
+		}
+	}
+
+	/**
      * オブジェクトからインスタンスを生成
      * @param o タイルオブジェクト
      * @param g タイルオブジェクトを所属しているグループ
      * @param state 登録するFlxState
      **/
-    private function _loadObject(o:TiledObject, g:TiledObjectGroup, state:PlayState):Void {
 
-    /*
+	private function _loadObject(o:TiledObject, g:TiledObjectGroup, state:PlayState):Void {
+
+		/*
         var px:Int = o.x;
         var py:Int = o.y;
 
@@ -170,33 +173,34 @@ class TiledLevel extends TiledMap {
             state.addGoal(px, py);
         }
     */
-    }
+	}
 
-    /**
+	/**
      * @param obj 当たり判定をするオブジェクト
      * @param notifyCallback ヒット時の処理
      * @param processCallback 当たり判定処理関数
      * @return 当たっていればtrue
      **/
-    public function collideWithLevel(obj:FlxObject, ?notifyCallback:FlxObject->FlxObject->Void, ?processCallback:FlxObject->FlxObject->Bool):Bool {
 
-        if(collidableTileLayers == null) {
-            // コリジョンがないので判定不要
-            return false;
-        }
+	public function collideWithLevel(obj:FlxObject, ?notifyCallback:FlxObject -> FlxObject -> Void, ?processCallback:FlxObject -> FlxObject -> Bool):Bool {
 
-        if(processCallback == null) {
-            processCallback = FlxObject.separate;
-        }
+		if(collidableTileLayers == null) {
+			// コリジョンがないので判定不要
+			return false;
+		}
 
-        for(map in collidableTileLayers) {
-            if(FlxG.overlap(map, obj, notifyCallback, processCallback)) {
-                // 当たった
-                return true;
-            }
-        }
+		if(processCallback == null) {
+			processCallback = FlxObject.separate;
+		}
 
-        // 当たっていない
-        return false;
-    }
+		for(map in collidableTileLayers) {
+			if(FlxG.overlap(map, obj, notifyCallback, processCallback)) {
+				// 当たった
+				return true;
+			}
+		}
+
+		// 当たっていない
+		return false;
+	}
 }
--- source/jp_2dgames/TmxLoader.hx
+++ source/jp_2dgames/TmxLoader.hx
@@ -4,113 +4,117 @@ import flixel.FlxG;
 import openfl.Assets;
 
 class TmxLoader {
-    private var _layers:Array<Layer2D>;
-    private var _tmpLayer:Layer2D;
-    private var _properties:Map<String, String>;
-    private var _width:Int = 0;
-    private var _height:Int = 0;
-    private var _tileWidth:Int = 0;
-    private var _tileHeight:Int = 0;
-    public var width(get_width, null):Int;
-    public var height(get_height, null):Int;
-    public var tileWidth(get_tileWidth, null):Int;
-    public var tileHeight(get_tileHeight, null):Int;
+	private var _layers:Array<Layer2D>;
+	private var _tmpLayer:Layer2D;
+	private var _properties:Map<String, String>;
+	private var _width:Int = 0;
+	private var _height:Int = 0;
+	private var _tileWidth:Int = 0;
+	private var _tileHeight:Int = 0;
+	public var width(get_width, null):Int;
+	public var height(get_height, null):Int;
+	public var tileWidth(get_tileWidth, null):Int;
+	public var tileHeight(get_tileHeight, null):Int;
 
-    public function new() {
-        // 読み込み失敗時のテンポラリ
-        _tmpLayer = new Layer2D();
-    }
+	public function new() {
+		// 読み込み失敗時のテンポラリ
+		_tmpLayer = new Layer2D();
+	}
 
-    /**
+	/**
      * Tiled Map Editorファイルをロードする
      * @param filepath *.tmxファイルのパス
      * @return Layer2D
      **/
-    public function load(filepath:String): Void {
 
-        _layers = new Array<Layer2D>();
-        _properties = new Map<String, String>();
+	public function load(filepath:String):Void {
 
-        var tmx:String = Assets.getText(filepath);
-        if(tmx == null) {
-            // 読み込み失敗
-            FlxG.log.warn("TmxLoader.load() tmx is null. file:'" + filepath + "''");
-            return;
-        }
+		_layers = new Array<Layer2D>();
+		_properties = new Map<String, String>();
 
-        // mapノード
-        var map:Xml = Xml.parse(tmx).firstElement();
-        _width = Std.parseInt(map.get("width"));
-        _height = Std.parseInt(map.get("height"));
-        _tileWidth = Std.parseInt(map.get("tilewidth"));
-        _tileHeight = Std.parseInt(map.get("tileheight"));
+		var tmx:String = Assets.getText(filepath);
+		if(tmx == null) {
+			// 読み込み失敗
+			FlxG.log.warn("TmxLoader.load() tmx is null. file:'" + filepath + "''");
+			return;
+		}
 
-        for (child in map.elements()) {
-            if (child.nodeName != "layer") { continue; }
-            // layerノード
-            var layer:Layer2D = new Layer2D();
-            var width = Std.parseInt(child.get("width"));
-            var height = Std.parseInt(child.get("height"));
-            layer.initialize(width, height);
-            for(gchild in child.elements()) {
+		// mapノード
+		var map:Xml = Xml.parse(tmx).firstElement();
+		_width = Std.parseInt(map.get("width"));
+		_height = Std.parseInt(map.get("height"));
+		_tileWidth = Std.parseInt(map.get("tilewidth"));
+		_tileHeight = Std.parseInt(map.get("tileheight"));
 
-                switch(gchild.nodeName) {
-                case "properties":
-                    for(prop in gchild.elements()) {
-                        var name = prop.get("name");
-                        var value = prop.get("value");
-                        _properties.set(name, value);
-                    }
-                case "data":
-                    var data:Xml = gchild;
-                    // CSVノード
-                    var csv:Xml = data.firstChild();
+		for(child in map.elements()) {
+			if(child.nodeName != "layer") { continue; }
+			// layerノード
+			var layer:Layer2D = new Layer2D();
+			var width = Std.parseInt(child.get("width"));
+			var height = Std.parseInt(child.get("height"));
+			layer.initialize(width, height);
+			for(gchild in child.elements()) {
 
-                    var text:String = csv.nodeValue;
-                    var y:Int = 0;
-                    for(line in text.split("\n")) {
-                        if(line == "") { continue; }
-                        var x:Int = 0;
-                        for(str in line.split(",")) {
-                            var val = Std.parseInt(str);
-                            if(val > 0) {
-                                layer.set(x, y, val);
-                            }
-                            x += 1;
-                        }
-                        y += 1;
-                    }
-                }
-            }
-            _layers.push(layer);
-        }
-    }
+				switch(gchild.nodeName) {
+					case "properties":
+						for(prop in gchild.elements()) {
+							var name = prop.get("name");
+							var value = prop.get("value");
+							_properties.set(name, value);
+						}
+					case "data":
+						var data:Xml = gchild;
+						// CSVノード
+						var csv:Xml = data.firstChild();
 
-    public function getLayerCount():Int {
-        return _layers.length;
-    }
+						var text:String = csv.nodeValue;
+						var y:Int = 0;
+						for(line in text.split("\n")) {
+							if(line == "") { continue; }
+							var x:Int = 0;
+							for(str in line.split(",")) {
+								var val = Std.parseInt(str);
+								if(val > 0) {
+									layer.set(x, y, val);
+								}
+								x += 1;
+							}
+							y += 1;
+						}
+				}
+			}
+			_layers.push(layer);
+		}
+	}
 
-    public function getLayer(idx:Int):Layer2D {
+	public function getLayerCount():Int {
+		return _layers.length;
+	}
 
-        if(_layers.length == 0) {
-            // 読み込めていない
-            FlxG.log.warn("TmxLoader.getLayer() _layers.length is 0.");
-            return _tmpLayer;
-        }
+	public function getLayer(idx:Int):Layer2D {
 
-        return _layers[idx];
-    }
+		if(_layers.length == 0) {
+			// 読み込めていない
+			FlxG.log.warn("TmxLoader.getLayer() _layers.length is 0.");
+			return _tmpLayer;
+		}
 
-    private function get_width() {
-        return _width;
-    }
-    private function get_height() {
-        return _height;
-    }
-    private function get_tileWidth() {
-        return _tileWidth;
-    }
-    private function get_tileHeight() {
-        return _tileHeight;
-    }
+		return _layers[idx];
+	}
+
+	private function get_width() {
+		return _width;
+	}
+
+	private function get_height() {
+		return _height;
+	}
+
+	private function get_tileWidth() {
+		return _tileWidth;
+	}
+
+	private function get_tileHeight() {
+		return _tileHeight;
+	}
 }
