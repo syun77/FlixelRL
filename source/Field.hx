060738b9957555e0c317525568398e1e74514685
syun77 <2dgames.jp@gmail.com>
2015-04-14 21:25:27 +0900

    fixed #15 HaxeFlixel_RogueLike-15 背景作成処理をFiledクラスに移行する

--- source/Field.hx
+++ source/Field.hx
@@ -3,6 +3,12 @@ package ;
 /**
  * フィールド管理
  **/
+import flixel.FlxG;
+import flash.geom.Point;
+import flash.geom.Rectangle;
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+import jp_2dgames.Layer2D;
 class Field {
 	// グリッドサイズ
 	public static inline var GRID_SIZE:Int = 32;
@@ -21,5 +27,34 @@ class Field {
 	}
 
 	public function new() {
+	}
+
+	public static function createBackground(layer:Layer2D):FlxSprite {
+		var spr = new FlxSprite();
+		var w = layer.width * GRID_SIZE;
+		var h = layer.height * GRID_SIZE;
+		// チップ画像読み込み
+		var chip = FlxG.bitmap.add("assets/images/wall1.png");
+		// 透明なスプライトを作成
+		spr.makeGraphic(w, h, FlxColor.TRANSPARENT);
+		// 転送先の座標
+		var pt = new Point();
+		// 転送領域の作成
+		var rect = new Rectangle(0, 0, GRID_SIZE, GRID_SIZE);
+		// 描画関数
+		var func = function(i:Int, j:Int, v:Int) {
+			pt.x = i * GRID_SIZE;
+			pt.y = j * GRID_SIZE;
+			if(v == 3) {
+				spr.pixels.copyPixels(chip.bitmap, rect, pt);
+			}
+		}
+
+		// レイヤーを走査する
+		layer.forEach(func);
+		spr.dirty = true;
+		spr.updateFrameData();
+
+		return spr;
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,6 @@
 package;
 
-import flash.geom.Rectangle;
-import flixel.util.loaders.CachedGraphics;
-import flash.geom.Point;
-import flixel.util.FlxColor;
 import flixel.FlxSprite;
-import flash.display.BitmapData;
 import jp_2dgames.TmxLoader;
 import flixel.FlxG;
 import flixel.FlxState;
@@ -26,22 +21,8 @@ class PlayState extends FlxState
 		var tmx = new TmxLoader();
 		tmx.load("assets/levels/001.tmx");
 		var layer = tmx.getLayer(0);
-		var pt:Point = new Point(0, 0);
-		var wall:CachedGraphics = new FlxSprite(0, 0, "assets/images/wall1.png").cachedGraphics;
-		var w = layer.width * Field.GRID_SIZE;
-		var h = layer.height * Field.GRID_SIZE;
-		var back = new FlxSprite().makeGraphic(w, h, FlxColor.TRANSPARENT);
-		var rect = new Rectangle(0, 0, Field.GRID_SIZE, Field.GRID_SIZE);
-		var func = function(i:Int, j:Int, v:Int) {
-			pt.x = i*Field.GRID_SIZE;
-			pt.y = j*Field.GRID_SIZE;
-			if(v == 3) {
-				back.pixels.copyPixels(wall.bitmap, rect, pt);
-			}
-		}
-		layer.forEach(func);
-		back.dirty = true;
-		back.updateFrameData();
+		// 背景画像を登録
+		var back = Field.createBackground(layer);
 		this.add(back);
 
 		// プレイヤー生成
