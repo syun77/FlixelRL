1b973b9ba534d2820f1ff49c51be6949d5cc3675
syun77 <2dgames.jp@gmail.com>
2015-07-07 18:26:17 +0900

    fixed #255 FlixelRL-255 反撃の鎧

--- source/jp_2dgames/game/Global.hx
+++ source/jp_2dgames/game/Global.hx
@@ -113,7 +113,6 @@ class Global {
         // りんごを持たせる
         var param = new ItemExtraParam();
         items.push(new ItemData(ItemConst.FOOD1, param));
-        items.push(new ItemData(ItemConst.ARMOR8, param));
       }
     }
     else {
