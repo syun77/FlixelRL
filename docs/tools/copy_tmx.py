60bb616c08f86de4f36f4531108cae532205e647
syun77 <kenmomomomomo@gmail.com>
2015-07-12 21:14:21 +0900

    fixed #447 FlixelRL-447 tmxコピーツールを作成する

---
+++ docs/tools/copy_tmx.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+import shutil
+
+# メイン処理
+def main():
+	# コピー元ファイル
+	src = "000.tmx"
+	# 開始番号
+	first = 16
+	last  = 20
+
+	# 実行しているスクリプトのパスを取得する
+	dir = os.path.dirname(__file__)
+	target_dir = dir + "/../../assets/levels/"
+
+	for i in range(first, last+1):
+		src_path = target_dir + src
+		dst_path = target_dir + "%03d.tmx"%i
+		shutil.copyfile(src_path, dst_path)
+		print "copy %s"%dst_path
+
+	print "done."
+
+if __name__ == '__main__':
+	main()
