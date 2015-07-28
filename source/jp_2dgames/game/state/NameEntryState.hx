d9fb23b40f9a889e9350abd2d4930cee4f00102c
syun77 <2dgames.jp@gmail.com>
2015-07-28 10:38:24 +0900

    fixed #581 FlixelRL-581 プレイヤーの名前入力画面の説明テキスト

--- source/jp_2dgames/game/state/NameEntryState.hx
+++ source/jp_2dgames/game/state/NameEntryState.hx
@@ -1,4 +1,8 @@
 package jp_2dgames.game.state;
+import flash.geom.Rectangle;
+import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import jp_2dgames.lib.CsvLoader;
 import jp_2dgames.game.GameData;
 import flixel.input.keyboard.FlxKey;
 import flixel.addons.ui.FlxButtonPlus;
@@ -9,13 +13,16 @@ import flixel.FlxState;
 
 private class MyButton extends FlxButtonPlus {
 
-  public function new(X:Float = 0, Y:Float = 0, ?Text:String, ?OnClick:Void->Void) {
+  public function new(X:Float = 0, Y:Float = 0, ?Text:String, ?OnClick:Void->Void, ?OnEnter:Void->Void, ?OnLeave:Void->Void) {
     var w = 160; // ボタンの幅
     var h = 32;  // ボタンの高さ
     var s = 16;  // フォントのサイズ
     super(X, Y, OnClick, Text, w, h);
     textNormal.size = s;
     textHighlight.size = s;
+
+    enterCallback = OnEnter;
+    leaveCallback = OnLeave;
   }
 }
 
@@ -27,29 +34,43 @@ class NameEntryState extends FlxState {
   // 名前に設定可能な最大文字数
   private static inline var MAX_NAME:Int = 8;
 
+  // ヘルプテキスト
+  private static inline var HELP_X = 200;
+  private static inline var HELP_Y = 60;
+
   // 名前テキストの座標
-  private static inline var NAME_X = 128;
+  private static inline var NAME_X = 256;
   private static inline var NAME_Y = 128;
-  private static inline var NAME_W = 256;
+  private static inline var NAME_W = 160;
 
   // 消去ボタン
-  private static inline var CLEAR_X = 60;
-  private static inline var CLEAR_Y = 180;
+  private static inline var CLEAR_X = NAME_X + 180;
+  private static inline var CLEAR_Y = NAME_Y;
 
   // 自動入力ボタン
-  private static inline var MALE_X = 60;
+  private static inline var MALE_X = 240;
   private static inline var MALE_Y = 260;
   private static inline var FEMALE_X = MALE_X + 200;
   private static inline var FEMALE_Y = MALE_Y;
 
   // 戻る
-  private static inline var BACK_X = 60;
+  private static inline var BACK_X = 320;
   private static inline var BACK_Y = 400;
 
   // 名前自動生成
   private var _generator:NameGenerator;
+  // 名前入力説明テキスト
+  private var _txtHelp:FlxText;
+  // 名前入力の枠
+  private var _sprName:FlxSprite;
   // 名前テキスト
   private var _txtName:FlxText;
+  // CSVテキスト
+  private var _csv:CsvLoader;
+  // ポップアップテキスト
+  private var _txtTip:FlxText;
+  // ポップアップの枠
+  private var _sprTip:FlxSprite;
 
   /**
    * 生成
@@ -57,29 +78,91 @@ class NameEntryState extends FlxState {
   override public function create():Void {
     super.create();
 
+    // CSVテキスト
+    _csv = new CsvLoader("assets/data/nameentry.csv");
+
+    // 名前説明
+    {
+      var msg = _csv.getString(1, "msg");
+      _txtHelp = new FlxText(HELP_X, HELP_Y, 480, msg);
+      _txtHelp.setFormat(Reg.PATH_FONT, Reg.FONT_SIZE);
+      this.add(_txtHelp);
+    }
+
+    // 自動入力
     _generator = new NameGenerator();
+
+    // 名前入力の枠
+    _sprName = new FlxSprite(NAME_X-8, NAME_Y-8);
+    _sprName.makeGraphic(NAME_W, Reg.FONT_SIZE*2, FlxColor.CORAL);
+    {
+      var w = _sprName.width;
+      var h = _sprName.height;
+      var rect = new Rectangle(2, 2, w-4, h-4);
+      _sprName.pixels.fillRect(rect, 0x802010);
+      _sprName.dirty = true;
+      _sprName.updateFrameData();
+    }
+    this.add(_sprName);
+
+    // 名前入力
     _txtName = new FlxText(NAME_X, NAME_Y, NAME_W, "", 24);
     this.add(_txtName);
+
+    // ポップアップテキスト
+    _txtTip = new FlxText(0, 0, 320, "");
+    _txtTip.setFormat(Reg.PATH_FONT, Reg.FONT_SIZE);
+    _txtTip.setBorderStyle(FlxText.BORDER_OUTLINE, FlxColor.GREEN);
+    _txtTip.color = FlxColor.WHITE;
+    _txtTip.visible = false;
+
+    _sprTip = new FlxSprite(0, 0);
+    _sprTip.makeGraphic(320, 24, FlxColor.BLACK);
+    _sprTip.alpha = 0.5;
+    _sprTip.visible = false;
 
     // 名前消去
     this.add(new MyButton(CLEAR_X, CLEAR_Y, "CLEAR", function() {
       _setName("");
+    }, function() {
+      _txtTip.visible = true;
+      _txtTip.text = _csv.getString(2, "msg");
+    }, function() {
+      _txtTip.visible = false;
     }));
 
     // 自動入力（男性）
     this.add(new MyButton(MALE_X, MALE_Y, "MALE", function() {
       _setName(_generator.getMale());
+    }, function() {
+      _txtTip.visible = true;
+      _txtTip.text = _csv.getString(3, "msg");
+    }, function() {
+      _txtTip.visible = false;
     }));
 
     // 自動入力（女性）
     this.add(new MyButton(FEMALE_X, FEMALE_Y, "FEMALE", function() {
       _setName(_generator.getFemale());
+    }, function() {
+      _txtTip.visible = true;
+      _txtTip.text = _csv.getString(4, "msg");
+    }, function() {
+      _txtTip.visible = false;
     }));
 
     // 戻るボタン
     this.add(new MyButton(BACK_X, BACK_Y, "BACK", function() {
       FlxG.switchState(new TitleState());
+    }, function() {
+      _txtTip.visible = true;
+      _txtTip.text = _csv.getString(5, "msg");
+    }, function() {
+      _txtTip.visible = false;
     }));
+
+    this.add(_sprTip);
+    this.add(_txtTip);
 
     _setName(GameData.getName());
   }
@@ -127,6 +210,12 @@ class NameEntryState extends FlxState {
    **/
   override public function update():Void {
     super.update();
+
+    _txtTip.x = FlxG.mouse.x;
+    _txtTip.y = FlxG.mouse.y-24;
+    _sprTip.x = _txtTip.x;
+    _sprTip.y = _txtTip.y;
+    _sprTip.visible = _txtTip.visible;
 
     _updateInput();
 #if debug
