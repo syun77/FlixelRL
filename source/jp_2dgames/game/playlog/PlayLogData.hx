acc74da3ca050cded12ace27c17fd13d61c12642
syun77 <kenmomomomomo@gmail.com>
2015-08-11 10:25:46 +0900

    fixed #692 FlixelRL-692 プレイログクラスの追加

---
+++ source/jp_2dgames/game/playlog/PlayLogData.hx
@@ -0,0 +1,23 @@
+package jp_2dgames.game.playlog;
+
+/**
+ * プレイログデータ
+ **/
+class PlayLogData {
+  public var user:String;
+  public var score:Int;
+  public var lv:Int;
+  public var floor:Int;
+  public var death:String;
+  public var playtime:Int;
+  public function new() {
+  }
+  public function copyFromDynamic(data:Dynamic):Void {
+    user     = data.user;
+    score    = data.score;
+    lv       = data.lv;
+    floor    = data.floor;
+    death    = data.death;
+    playtime = data.playtime;
+  }
+}
