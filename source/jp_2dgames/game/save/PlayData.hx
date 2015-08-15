a128516a66d79b770ab353ce20b178c4dce3c0f6
syun77 <kenmomomomomo@gmail.com>
2015-08-15 15:25:36 +0900

    fixed #702 FlixelRL-702 プレイデータの保存

---
+++ source/jp_2dgames/game/save/PlayData.hx
@@ -0,0 +1,60 @@
+package jp_2dgames.game.save;
+
+/**
+ * ゲームプレイデータ
+ **/
+class PlayData {
+  // トータルプレイ時間(秒)
+  public var playtime:Float = 0;
+  // プレイ回数
+  public var cntPlay:Int = 0;
+  // ゲームクリア回数
+  public var cntGameclear:Int = 0;
+  // 最大スコア
+  public var maxScore:Int = 0;
+  // フロア最大到達数
+  public var maxFloor:Int = 1;
+  // 最大レベル
+  public var maxLv:Int = 1;
+  // 最大所持金
+  public var maxMoney:Int = 0;
+  // 最大アイテム所持数
+  public var maxItem:Int = 0;
+  // アイテム獲得フラグ
+  public var flgItemFind:Array<Int>;
+  // 敵の撃破数
+  public var cntEnemyKill:Int = 0;
+  // 敵の撃破フラグ
+  public var flgEnemyKill:Array<Int>;
+
+  /**
+   * コンストラクタ
+   **/
+  public function new() {
+    flgEnemyKill = new Array<Int>();
+    flgItemFind  = new Array<Int>();
+  }
+
+  /**
+   * コピー
+   **/
+  public function copyFromDynamic(data:Dynamic) {
+    playtime     = data.playtime;
+    cntPlay      = data.cntPlay;
+    cntGameclear = data.cntGameclear;
+    maxScore     = data.maxScore;
+    maxFloor     = data.maxFloor;
+    maxLv        = data.maxLv;
+    maxMoney     = data.maxMoney;
+    maxItem      = data.maxItem;
+    var itemidList = cast(data.flgItemFind, Array<Int>);
+    for(itemid in itemidList) {
+      flgItemFind.push(itemid);
+    }
+    cntEnemyKill = data.cntEnemyKill;
+    var enemyidList = cast(data.flgEnemyKill, Array<Int>);
+    for(enemyid in enemyidList) {
+      flgEnemyKill.push(enemyid);
+    }
+  }
+}
